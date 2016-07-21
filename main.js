aa1e7989ec77f5e472d5457c610e30d1c615902c
Benjamin Huang <coolconfucius@yahoo.com>
2016-07-20 21:42:17 -0700

    init function

--- main.js
+++ main.js
@@ -1,58 +1,70 @@
-var grid = []; 
-
-function makeGrid(){
-  for(let i = 0; i < 16; i++){
-    var tile = {
-      tileId: "t"+i,
-      hex: "#ffffff",
-      isNew: true, 
-      secondsPassedSinceColorChange: 0
-    }
-    grid.push(tile);    
-  }
-};
-
-makeGrid(); 
-
-
-setInterval(function(){ 
-  for(let i in grid){
-    grid[i].secondsPassedSinceColorChange += 0.25;    
-  }
-  pickRand(); 
-}, 250);
-
-function pickRand(){
-  var rand = Math.floor(Math.random() * 16 ); 
-  if (grid[rand].isNew) {
-    grid[rand].isNew = false; 
-    changeColor(rand); 
+function ready(init) {
+  if (document.readyState != 'loading'){
+    init();
   } else {
-    if (checkSecondsPassedSinceColorChange(rand)) {
-      changeColor(rand);       
+    document.addEventListener('DOMContentLoaded', init);
+  }
+}
+
+function init(){
+  
+  var pause = false; 
+  var grid = []; 
+
+  function makeGrid(){
+    for(let i = 0; i < 16; i++){
+      var tile = {
+        tileId: "t"+i,
+        hex: "#ffffff",
+        isNew: true, 
+        secondsPassedSinceColorChange: 0
+      }
+      grid.push(tile);    
+    }
+  };
+
+  makeGrid(); 
+
+  setInterval(function(){ 
+    for(let i in grid){
+      grid[i].secondsPassedSinceColorChange += 0.25;    
+    }
+    pickRand(); 
+  }, 250);
+
+  function pickRand(){
+    var rand = Math.floor(Math.random() * 16 ); 
+    if (grid[rand].isNew) {
+      grid[rand].isNew = false; 
+      changeColor(rand); 
     } else {
-      pickRand(); 
+      if (checkSecondsPassedSinceColorChange(rand)) {
+        changeColor(rand);       
+      } else {
+        pickRand(); 
+      }
     }
-  }
 
-};
+  };
 
-function changeColor(tileNumber){
-  document.getElementById("t"+tileNumber).style.backgroundColor = makeRGB();
-  grid[tileNumber].secondsPassedSinceColorChange = 0; 
-};
+  function changeColor(tileNumber){
+    document.getElementById("t"+tileNumber).style.backgroundColor = makeRGB();
+    grid[tileNumber].secondsPassedSinceColorChange = 0; 
+  };
 
-function makeRGB(){
-	var hexString = (Math.floor(Math.random() * (parseInt('0xFFFFFF', 16) + 1))).toString(16);
-	return "#" + ('00000' + hexString).slice(-6);
-};
+  function makeRGB(){
+    var hexString = (Math.floor(Math.random() * (parseInt('0xFFFFFF', 16) + 1))).toString(16);
+    return "#" + ('00000' + hexString).slice(-6);
+  };
 
-function checkSecondsPassedSinceColorChange (tileNumber){
-  if (grid[tileNumber].secondsPassedSinceColorChange >= 2) {
-    console.log("true");
-    return true; 
-  } else {
-    console.log("false");
-    return false; 
-  }  
-};
+  function checkSecondsPassedSinceColorChange (tileNumber){
+    if (grid[tileNumber].secondsPassedSinceColorChange >= 2) {
+      console.log("true");
+      return true; 
+    } else {
+      console.log("false");
+      return false; 
+    }  
+  };
+  
+}
